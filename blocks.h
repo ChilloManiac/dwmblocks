90df1cce828029d3a9bce77f5375cae056435f30
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-08-10 14:39:27 +0200

    Emptyblock prefix

--- blocks.h
+++ blocks.h
@@ -4,14 +4,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "cdmblockstest.sh 1",              120,    1}, EMPTYBLOCK,
-    {"", "cdmblockstest.sh 2",              120,    2}, EMPTYBLOCK,
-    {"", "cdmblockstest.sh 3",              120,    3}, EMPTYBLOCK,
-    {"", "cdmblockstest.sh 4",              120,    4}, EMPTYBLOCK,
-    {"", "cdmblockstest.sh 5",              120,    5}, EMPTYBLOCK,
-    {"", "cdmblockstest.sh 6",              120,    6}, EMPTYBLOCK,
-    {"", "cdmblockstest.sh 7",              120,    7}, EMPTYBLOCK,
-    {"", "cdmblockstest.sh 8",              120,    8}, EMPTYBLOCK,
+    EMPTYBLOCK,
+    {"", "charddrivestat.sh",               3600,   4}, EMPTYBLOCK,
+    {"", "cmemstat.sh",                     60,     3}, EMPTYBLOCK,
+    {"", "ccpustat.sh",                     10,     2}, EMPTYBLOCK,
+    {"", "cvolget.sh",                      0,      1}, EMPTYBLOCK, 
+    {"", "cweather.sh",                     600,    5}, EMPTYBLOCK,
+    {"", "cclockget.sh",                    60,     6}, EMPTYBLOCK,
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
